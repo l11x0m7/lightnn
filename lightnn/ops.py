0dbcd32d4f4eba1aead76c47a111358c0e359fd0
lxm <l11x0m7@bupt.edu.cn>
2018-01-23 12:45:04 +0800

    update

--- lightnn/ops.py
+++ lightnn/ops.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+
 def _check_convolution_layer(filter_size, filter_num, zero_padding, stride):
     # if not isinstance(input_height, int):
     #     raise ValueError('`input_height` must be int')
